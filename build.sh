6706e96e4150996853a42371ff88e8450495e3fb
PatrickLTew <41203682+PatrickLTew@users.noreply.github.com>
2021-01-04 17:30:59 +0000

    Muted the command that runs the migration scripts from TewChat, since Render's postgresql db wasn't allowing TewChat to modify it, given that it was set up by TewChat_1_1

--- build.sh
+++ build.sh
@@ -17,6 +17,12 @@ cd ../../
 # Build the release and overwrite the existing release directory
 MIX_ENV=prod mix release --overwrite
 
-#Run the scripts to migrate the database
-_build/prod/rel/prod/bin/prod eval "DB.Release.migrate"
+#Run the scripts to migrate the database:
+#NOTE - once I got it working via Tew_Chat_1_1, other 'apps' (clones of TewChat) pointing to the same database were nolonger being accepted, and causing errors. It said one of the problems could be that the tables one is trying to migrate were set up by a different 'library'. Probably the other app (Tew_Chat_1_1).
+#So, I have commented out the migration script activation command here, since we don't technically need it, right now.
+#To do further migrations, of coure we /would/ need to fix this problem.
+#My idea is to go into TewChat_1_1 on the Render Dashboad, drop the database in the shell there via mix.ecto.drop
+#Then run the migration script again underneath here, when redeploying TewChat
+#But for now, I'll leave it, and help clean the house :)
+#_build/prod/rel/prod/bin/prod eval "DB.Release.migrate"
 echo "App Built <3"
