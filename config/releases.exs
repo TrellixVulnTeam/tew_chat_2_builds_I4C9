a2d47018ba484438397b33266316180dd7780e60
PatrickLTew <41203682+PatrickLTew@users.noreply.github.com>
2021-01-05 14:15:37 +0000

    Fixed syntax error in releases.exs where the final ) at the end of the pool_size: String.to_integer(.... was missing

--- config/releases.exs
+++ config/releases.exs
@@ -40,7 +40,7 @@ database_url =
  config :db, DB.Repo,
  # ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "50")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "50"))
 ########################################################################
 
 
